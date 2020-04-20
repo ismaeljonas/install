3554c53fa0709cc1e97daa1d766851f973af54dd
IsmaelJonas <leamsi.jonas@gmail.com>
2020-04-20 20:07:36 -0300

    Alterar readme

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
+## Motivação
+
+## Atenção
+Este script inicialmente é a gambiarra da gambiarra, a ideia é usá-lo para aprender a usar o Git então use por conta e risco
+
+## Como Usar
+```bash
+Git clone https://github.com/ismaeljonas/install.git
+cd install
+chmod +x install.sh 
+sudo sh install.sh
+```
 
 
-
-
+---
 ## Contributing
 
 1. Faça o _fork_ do projeto (<https://github.com/yourname/yourproject/fork>)
-2. Crie uma _branch_ para sua modificação (`git checkout -b feature/feature`)
+2. Crie uma _branch_ para sua modificação (`git checkout -b feature`)
 3. Faça o _commit_ (`git commit -am 'Add some feature'`)
-4. _Push_ (`git push origin feature/feature`)
+4. _Push_ (`git push origin feature`)
 5. Crie um novo _Pull Request_
+
+## Inspiração
+https://gomex.me/2020/01/03/what-i-should-install-on-my-mac/
+https://github.com/ibrunotome/macOS-switchblade/blob/master/install.sh
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # Autor: Ismael Ferreira
+# based Debian SO
 
 #add repository
 echo "Add Repository, update and upgrade........................"
@@ -8,9 +9,6 @@ add-apt-repository ppa:obsproject/obs-studio #obs studio
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list # Add the Cloud SDK distribution URI as a package source
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt-get update && apt-get upgrade
-
-#Installing VirtualBox
-echo "Installing VirtualBox........................"
 
 #Installing kubectl https://kubernetes.io/docs/getting-started-guides/kubectl/
 echo "Installing kubectl..........................."
@@ -23,9 +21,6 @@ echo "Installing minikube.........................."
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.0/minikube-linux-amd64 
 chmod +x minikube 
 mv minikube /usr/local/bin/
-
-echo "Installing Obs Studio.........................."
-#git curl
 
 echo "Installing GIT, Curl, ZSH, Google Cloud SDK, Virtualbox and OBS Studio .........................."
 apt-get install git -y
@@ -45,6 +40,12 @@ snap install --classic code #vscode
 
 #bash <(curl -s -S -L https://git.io/install-gam)
 #bash <(curl -s -S -L https://get.docker.com)
-
+# # POST INSTALLATION
+# create docker group
+#sudo groupadd docker
+# add your user to the docker group.
+#sudo usermod -aG docker $USER
+# configure to star on boot
+#sudo systemctl enable docker
 # Install Oh My Zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
