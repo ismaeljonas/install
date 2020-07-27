c637ed0e95687a3294968932367bcbd9eee55f6f
ismaeljonas <38139392+ismaeljonas@users.noreply.github.com>
2020-07-26 21:46:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # based Debian SO
 
 echo "Add Repository, update and upgrade OS........................"
-add-apt-repository ppa:obsproject/obs-studio #obs studio
+#add-apt-repository ppa:obsproject/obs-studio #obs studio
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list # Add the Cloud SDK distribution URI as a package source
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
